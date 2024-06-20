9ca4989902b773c2269ad3aece1c3fe82b5ae103
remco van der Velde <remco.pc@outlook.com>
2024-06-20 15:34:26 +0200

    added -skip=cache-clear

--- start.sh
+++ start.sh
@@ -5,16 +5,6 @@ echo "composer install -n" >> ~/.bashrc
 echo "cp /Application/vendor/r3m_io/framework/Bin/R3m.php /Application/Bin/R3m.php" >> ~/.bashrc
 echo "php /Application/Bin/R3m.php bin app" >> ~/.bashrc
 echo "app install r3m_io/boot -skip=cache-clear" >> ~/.bashrc
-#echo "app install r3m_io/node" >> ~/.bashrc
-#echo "app install r3m_io/route" >> ~/.bashrc
-#echo "app install r3m_io/event" >> ~/.bashrc
-#echo "app install r3m_io/middleware" >> ~/.bashrc
-#echo "app install r3m_io/output_filter" >> ~/.bashrc
-#echo "app install r3m_io/config" >> ~/.bashrc
-#echo "app install r3m_io/autoload" >> ~/.bashrc
-#echo "app install r3m_io/log" >> ~/.bashrc
-#echo "app install r3m_io/host" >> ~/.bashrc
-#echo "app init --url=/mnt/Vps2/Data/Init/Init" >> ~/.bashrc
 echo "chown root:root /Application/start.sh" >> ~/.bashrc
 echo "chown root:root /Application/enter.sh" >> ~/.bashrc
 echo "chown root:root /Application/install.dev.sh" >> ~/.bashrc
